4c568546b7337b03f7b781b598851e449e509bdb
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-07-06 15:53:15 +0600

    Contrl Flow Lab completed

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,67 @@
+let userRole = "admin";
+let accessLevel;
+
+if (userRole === "admin") {
+    accessLevel = "Full access granted";
+} else if (userRole === "manager") {
+    accessLevel = "Limited access granted";
+} else {
+    accessLevel = "No access granted";
+}
+
+console.log("Access Level:", accessLevel);
+
+let isLoggedIn = true;
+let userMessage;
+
+if (isLoggedIn) {
+    if (userRole === "admin") {
+        userMessage = "Welcome, Admin!";
+    } else {
+        userMessage = "Welcome, User!";
+    }
+} else {
+    userMessage = "Please log in to access the system.";
+}
+
+console.log("User Message:", userMessage);
+
+let userType = "subscriber";
+let userCategory;
+
+switch (userType) {
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Unknown";
+}
+
+console.log("User Category:", userCategory);
+
+let isAuthenticated = true;
+
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+
+console.log("Authentication Status:", authenticationStatus);
+
+
+// Practice
+let person = "Employee"
+
+if (person == "Employee") {
+    console.log('You are authorized to have access to Dietary Services');
+} else if (person == "Enrolled Member") {
+    console.log('You are authorized to have access to "Dietary Services" and one-on-one interaction with a dietician.');
+} else if (person == "Subscriber") {
+    console.log('You are authorized to have partial access to facilitate "Dietary Services" only.');
+} else if (person == "Non-Subscriber") {
+    console.log('Sorry! You are not eligible for the facility');
+    
+}
